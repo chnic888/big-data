a96d14027887edf243011193e8f793736f4c9caf
chnic888 <chnic888@gmail.com>
2020-04-22 02:54:33 +0800

    add avro sample

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## Hadoop
 Code snippet of **_Hadoop: The Definitive Guide, 4th Edition_**
 
-### Running m/r locally
+### Running m/r samples locally
 ```bash
 #build jar file
 ./gradlew clean build
@@ -23,4 +23,19 @@ hadoop jar ./build/libs/ncdc-1.0.jar com.chnic.mapreduce.NCDCMaxTemperatureSorte
 
 #run avro test case only 
 ./gradlew clean test --tests com.chnic.avro.*
+```
+
+#### Running Avro m/r sample locally
+```bash
+#build jar file
+./gradlew clean build
+
+#export LIBJARS
+export LIBJARS=./build/libs/avro-mapred-1.7.7-hadoop2.jar
+
+#export HADOOP_CLASSPATH
+export HADOOP_CLASSPATH=./build/libs/avro-mapred-1.7.7-hadoop2.jar
+
+#run m/r
+hadoop jar ./build/libs/ncdc-1.0.jar com.chnic.mapreduce.NCDCAvroMaxTemperature -libjars ${LIBJARS} './src/test/resources/190*.gz' ./out
 ```
---
+++ src/main/java/com/chnic/mapreduce/NCDCAvroMaxTemperature.java
@@ -0,0 +1,98 @@
+package com.chnic.mapreduce;
+
+import com.chnic.avro.YearTemperature;
+import org.apache.avro.Schema;
+import org.apache.avro.mapred.AvroKey;
+import org.apache.avro.mapred.AvroValue;
+import org.apache.avro.mapreduce.AvroJob;
+import org.apache.avro.mapreduce.AvroKeyOutputFormat;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class NCDCAvroMaxTemperature extends Configured implements Tool {
+
+    static class MapClass extends Mapper<LongWritable, Text, AvroKey<Integer>, AvroValue<YearTemperature>> {
+
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            try {
+                Map.Entry<Integer, Float> entry = RowDataParser.parser(value.toString());
+
+                YearTemperature record = new YearTemperature();
+                record.setYear(entry.getKey());
+                record.setTemperature(entry.getValue());
+
+                context.write(new AvroKey<>(entry.getKey()), new AvroValue<>(record));
+            } catch (ValidateException e) {
+                context.getCounter(ValidateResult.valueOf(e.getMessage())).increment(1);
+            }
+        }
+    }
+
+    static class ReduceClass extends Reducer<AvroKey<Integer>, AvroValue<YearTemperature>, AvroKey<YearTemperature>, NullWritable> {
+
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+            super.setup(context);
+        }
+
+        @Override
+        protected void reduce(AvroKey<Integer> key, Iterable<AvroValue<YearTemperature>> values, Context context) throws IOException, InterruptedException {
+            float maxValue = Float.MIN_VALUE;
+            YearTemperature finalRecord = null;
+
+            for (AvroValue<YearTemperature> value : values) {
+                YearTemperature genericRecord = value.datum();
+                if (genericRecord.getTemperature() > maxValue) {
+                    maxValue = genericRecord.getTemperature();
+                    finalRecord = genericRecord;
+                }
+            }
+
+            context.write(new AvroKey<>(finalRecord), NullWritable.get());
+        }
+    }
+
+    @Override
+    public int run(String[] strings) throws Exception {
+        Job job = Job.getInstance(getConf(), "Compute Max Temperature by Avro");
+        job.setMapperClass(MapClass.class);
+        job.setJarByClass(NCDCAvroMaxTemperature.class);
+
+        job.setReducerClass(ReduceClass.class);
+
+        AvroJob.setMapOutputKeySchema(job, Schema.create(Schema.Type.INT));
+        AvroJob.setMapOutputValueSchema(job, YearTemperature.getClassSchema());
+
+        AvroJob.setOutputKeySchema(job, YearTemperature.getClassSchema());
+        AvroJob.setOutputValueSchema(job, Schema.create(Schema.Type.NULL));
+
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(AvroKeyOutputFormat.class);
+
+        FileInputFormat.setInputPaths(job, new Path(strings[0]));
+        FileOutputFormat.setOutputPath(job, new Path(strings[1]));
+
+        return job.waitForCompletion(true) ? 0 : 1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int result = ToolRunner.run(new Configuration(), new NCDCAvroMaxTemperature(), args);
+        System.exit(result);
+    }
+}
