d5a33f98b0ddb81acd82c1901003e2dd4fa0c88c
chnic888 <chnic888@gmail.com>
2020-05-30 18:27:08 +0800

    add spark stream sample

--- src/main/java/com/chnic/spark/SparkStreamingQuery.java
+++ src/main/java/com/chnic/spark/SparkStreamingQuery.java
@@ -1,20 +1,26 @@
 package com.chnic.spark;
 
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.ForeachWriter;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.streaming.StreamingQuery;
 import org.apache.spark.sql.streaming.StreamingQueryException;
+import org.apache.spark.sql.streaming.Trigger;
 import org.apache.spark.sql.types.*;
 
 import java.util.concurrent.TimeoutException;
 
 import static org.apache.spark.sql.functions.col;
+import static org.apache.spark.sql.functions.expr;
 
 public class SparkStreamingQuery {
 
     public static void main(String[] args) throws TimeoutException, StreamingQueryException {
-        SparkSession spark = SparkSession.builder().appName("Spark streaming demo").getOrCreate();
+        args = new String[]{
+                "/home/nick/code/big-data/src/test/resources/streaming-data/"
+        };
+
+        SparkSession spark = SparkSession.builder().appName("Spark streaming demo").master("local[*]").getOrCreate();
         spark.sparkContext().setLogLevel("error");
 
         StructType schema = new StructType(new StructField[]{
@@ -32,8 +38,46 @@ public class SparkStreamingQuery {
         });
 
         Dataset<Row> streaming = spark.readStream().schema(schema).option("maxFilesPerTrigger", 1).json(args[0]);
-        Dataset<Row> activityCounts = streaming.groupBy(col("gt")).count();
-        StreamingQuery activityQuery = activityCounts.writeStream().queryName("activity_counts").format("console").outputMode("complete").start();
-        activityQuery.awaitTermination();
+
+//        streaming.groupBy(col("gt")).count()
+//                .writeStream().queryName("activity_counts").format("console").outputMode("complete")
+//                .start()
+//                .awaitTermination();
+
+//        streaming.withColumn("stairs", expr("gt like '%stairs%'"))
+//                .where("stairs")
+//                .where("gt is not null")
+//                .select("gt", "Model", "Arrival_Time", "Creation_Time")
+//                .writeStream().queryName("simple_transform").format("console").outputMode("append")
+//                .start()
+//                .awaitTermination();
+
+//        streaming.cube(col("gt"), col("Model")).avg("x", "y", "z")
+//                .writeStream().queryName("device_counts").format("console").outputMode("complete")
+//                .start()
+//                .awaitTermination();
+
+        streaming.cube(col("gt"), col("Model")).avg("x", "y", "z")
+                .writeStream().trigger(Trigger.ProcessingTime("2 seconds")).queryName("device_counts").format("console").outputMode("complete")
+                .start()
+                .awaitTermination();
+
+//        streaming.writeStream().queryName("foreach sink").foreach(new ForeachWriter<Row>() {
+//            @Override
+//            public boolean open(long partitionId, long epochId) {
+//                System.out.println(partitionId + " : " + epochId);
+//                return true;
+//            }
+//
+//            @Override
+//            public void close(Throwable errorOrNull) {
+//
+//            }
+//
+//            @Override
+//            public void process(Row value) {
+//                System.out.println(value.json());
+//            }
+//        }).start().awaitTermination();
     }
 }
